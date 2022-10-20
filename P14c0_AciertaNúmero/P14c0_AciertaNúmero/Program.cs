f5716633493c3ffa91687e9d0917047e47a79af8
mnocea <mnoceacabrera@gmail.com>
2022-10-20 14:21:32 +0200

    asdfasdffasdfas

---
+++ P14c0_AciertaNúmero/P14c0_AciertaNúmero/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P14c0_AciertaNúmero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
