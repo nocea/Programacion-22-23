f74a5d0c4148034d621da4a9b7d0308ceabc60fa
mnocea <mnoceacabrera@gmail.com>
2022-10-31 11:57:22 +0100

    asdfas

--- P14f_SumadorYMedia/P14f_SumadorYMedia/Program.cs
+++ P14f_SumadorYMedia/P14f_SumadorYMedia/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,25 +10,29 @@ namespace P14f_SumadorYMedia
     internal class Program
     {
         static void Main(string[] args)
-        { 
-            int suma = 0, num, contador = 0;
-            string captura;
+        {
+            int suma = 0, num,contador=0;
+            int min = -1000, max = 1000;
+            bool ok;
+           
+            Console.Write("\n\tDime un numero,intro para salir: ");
+            ok = Int32.TryParse(Console.ReadLine(), out num);
+            while (ok && (num > min && num < max))
+                {
+                suma += num;
+                contador++;
+                Console.Write("\n\tDime un numero,intro para salir: ");
+                ok = Int32.TryParse(Console.ReadLine(), out num);
 
-            Console.Write("\n\tDime sumando {0} (intro=salir): ", contador + 1);
-            captura = Console.ReadLine();
-            while (captura != "")
-            {
-                num = Convert.ToInt32(captura);
-                suma += num;   // <-- Acumulamos el número
-                contador++;    // <-- incrementamos el contador
-                               // y pedimos el siguiente
-                Console.Write("\n\tDime sumando {0} (intro=salir): ", contador + 1);
-                captura = Console.ReadLine();
+
             }
-            double media = 1.0*suma / contador;
-            Console.WriteLine("Suma: "+suma);
-            Console.WriteLine("Media: "+media);
-            Console.WriteLine("Pulse intro para salir");
+
+            double media=(double)(suma/contador);
+            Console.WriteLine("Suma= {0}", suma);
+            Console.WriteLine("Media= {0}",media);
+
+            
+            Console.WriteLine("Pulse cualquier tecla para salir");
             Console.ReadLine();
         }
     }
---
+++ P14h_MultiplosEnColumnas/P14h_MultiplosEnColumnas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace P14h_MultiplosEnColumnas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce la cantidad de numeros a presentar");
+            int cant =
+
+            Console.WriteLine("Introduce el numero del que vamos a sacar sus multiplos");
+            int num=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce el valor a partir del cual vamos a hallar sus multiplos");
+            int min=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce un número de columnas al presentar");
+            int nc=Convert.ToInt32(Console.ReadLine()); 
+        }
+    }
+}
