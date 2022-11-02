d7a3169be98ac625d0cb4411fafd77ba9240cf9e
mnocea <mnoceacabrera@gmail.com>
2022-11-02 09:19:50 +0100

    Correcion sumador y media

--- P14f_SumadorYMedia/P14f_SumadorYMedia/Program.cs
+++ P14f_SumadorYMedia/P14f_SumadorYMedia/Program.cs
@@ -10,23 +10,43 @@ namespace P14f_SumadorYMedia
     {
         static void Main(string[] args)
         {
-            int suma = 0, num, contador = 0;
-            bool ok = true;
-
-            while (ok)
+            int suma = 0, num, contador = 0; ;
+            bool ok;
+            string captura;//Se declara la variable captura para poder obtner un valor vacio
+            Console.WriteLine("\t\t--Introduce numero para sumar,Intro para salir--");
+            
+            do
             {
-                Console.WriteLine("Dime un numero,intro para salir");
-                ok = Int32.TryParse(Console.ReadLine(), out num);
-                suma += num;
-                if (ok) { contador++; }
+                Console.Write("NÚMERO {0}: ", contador + 1);//Se pone el contador +1 pero no se incrementa y se guarda saldria mas uno en todos los intentos.
+                captura = Console.ReadLine();//se captura una primera vez.
+                if (captura != "")//Si no está vacío entra
+                {
+                    ok = Int32.TryParse(captura, out num);//intenta guardar captura en num.
+                    if (ok)//Si puede lo suma e incrementa
+                    {
+                        suma += num;
+                        contador++;
+                    }
+                    else//si no puede guardarlo se da el mensaje de error
+                    {
+                        Console.WriteLine("Error de formato");
+                    }
+                }
 
-                Console.WriteLine(contador);
+
+
+            } while (captura!="");//Se repite siempre que no sea vacio.
+
+            if (contador > 0)//control si el contador no se ha incrementado (si se ha introducido algun valor valido se hace la suma y media)
+            {
+                double media = 1.0 * suma / contador;
+                Console.WriteLine("Suma: " + suma);
+                Console.WriteLine("Media: " + media);
             }
-
-
-            double media = 1.0 * suma / contador;
-            Console.WriteLine("Suma: " + suma);
-            Console.WriteLine("Media: " + media);
+            else
+            {
+                Console.WriteLine("No hay nada que sumar");
+            }
             Console.WriteLine("Pulse intro para salir");
             Console.ReadLine();
         }
