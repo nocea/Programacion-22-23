600bce790672ee4c599ac1e00e20e584cf4fc03b
mnocea <mnoceacabrera@gmail.com>
2022-11-09 10:13:27 +0100

    CapturaEntero

---
+++ P21a_CapturaEntero/P21a_CapturaEntero/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P21a_CapturaEntero
+{
+    internal class Program
+    {
+        const int MIN = 100, MAX = 200;
+        static void Main(string[] args)
+        {
+            string txt = string.Format("Dime un numero entre[{0}...{1}]", MIN, MAX);
+            CapturaEntero(txt,MIN,MAX);
+            Console.WriteLine("Pulse intro para salir");
+            Console.ReadLine();
+        }
+        static int CapturaEntero(string texto,int min,int max)
+        {
+            int numero;
+            bool ok;
+            do
+            {
+                Console.WriteLine(texto);
+                ok = Int32.TryParse(Console.ReadLine(), out numero);
+                if (!ok)
+                    Console.WriteLine("**Error de Formato**");
+            } while (!ok || numero < min || numero > max);
+
+            return numero;
+        }
+    }
+}
