95ee7ce707307254ea02807320c76cad82ccf155
mnocea <mnoceacabrera@gmail.com>
2022-11-22 13:08:11 +0100

    dsdsfdfsdfsdfsdfsdfdfsdfsdfs

---
+++ P21-30/P22d_VectorNones/P22d_VectorNones/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P22d_VectorNones
+{
+    internal class Program
+    {   static int[] vNones;
+        static void Main(string[] args)
+        {
+            
+            int tamaño = CapturaEntero("Introduce el tamaño de la tabla", 5, 100);
+
+            
+            
+        }
+        static CargaTablaNones() { 
+        }
+        static int CapturaEntero(string txt, int min, int max)
+        {
+            int numero;
+            bool ok;
+            do
+            {
+                Console.Write("{0} [{1}...{2}]: ", txt, min, max);
+                ok = Int32.TryParse(Console.ReadLine(), out numero);
+                Console.Clear();
+                if (!ok)
+                    Console.WriteLine("**Error de Formato**");
+                else if (numero < min || numero > max)
+                {
+                    Console.WriteLine("**Error:Numero fuera de rango**");
+                    ok = false;
+                }
+            } while (!ok);
+            //comentario
+            return numero;
+        }
+    }
+}
