e73ed10bf26095897bcc316e564c850688309be7
mnocea <mnoceacabrera@gmail.com>
2022-12-12 14:41:01 +0100

    adasd

---
+++ P21-30/P23a2_PruebasMétodos/P23a2_PruebasMétodos/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class PruebasMetodosPosicion
+{
+    static void Main(string[] args)
+    {
+        #region--- No tocar este código -------
+        for (int f = 10; f < 20; f++)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.SetCursorPosition(0, f);
+            Console.Write("{0}                                                                              ", f);
+            Console.ResetColor();
+            
+            
+        }
+        #endregion
+
+        //----- Escribe a partir de esta línea tu código de prueba -------
+        Print("Pulsa para seguir",true,14);
+
+        Console.Write("\n\n\n\t\tPulsa una tecla para salir:");
+        Console.ReadKey();
+    }
+
+    // Escribe aquí el método
+    static void LimpiaFila(int fila)
+    {
+        Console.SetCursorPosition(0, fila);
+        Console.Write("                                                                                    ");
+        Console.SetCursorPosition(0, fila);
+         
+    }
+    static void Pausa(string txt,int fila)
+    {
+        Console.SetCursorPosition(0, fila);
+        Console.Write("                                                                                    ");
+        Console.SetCursorPosition(0, fila);                                                                       
+        Console.Write("Pulsa una tecla para {0}", txt);
+        Console.ReadKey();
+        Console.SetCursorPosition(0, fila);
+        Console.Write("                                                                                    ");
+    }
+    static void Print(string txt,bool conReadKey,int fila)
+    {
+        Console.SetCursorPosition(0, fila);
+        Console.Write("                                                                                    ");
+        Console.SetCursorPosition(0, fila);
+        Console.Write(txt);
+        if (conReadKey) {
+            Console.ReadKey();
+        }
+    }
+}
+
