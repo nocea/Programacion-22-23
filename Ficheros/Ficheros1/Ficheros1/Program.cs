0e4f3b63cd206f1e449d8fa55fbdb8f2aa7e356c
mnocea <mnoceacabrera@gmail.com>
2023-01-09 11:50:48 +0100

    ficheros

---
+++ Ficheros/Ficheros1/Ficheros1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ficheros1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //En lugar de poner la doble barra se puede poner @ antes para evitar los caracteres especiales 
+            //Seria:@"C:\\Users\\Mario Nocea\\Desktop\\zDatosPrueba\\texto.txt"
+            StreamWriter sw = File.CreateText("C:\\Users\\Mario Nocea\\Desktop\\zDatosPrueba\\prueba.txt"); 
+        }
+    }
+}
---
+++ P21-30/Pruebaqasadsa/Pruebaqasadsa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebaqasadsa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tabla = new int[100];
+            List<int> list = new List<int>();
+            list=tabla.ToList();    
+            tabla=list.ToArray();
+            char x=Console.ReadKey().KeyChar;  
+        }
+    }
+}
