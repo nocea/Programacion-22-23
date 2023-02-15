ba7e122ff3089986426c920c125da3d535fe9c55
mnocea <mnoceacabrera@gmail.com>
2023-02-15 14:24:55 +0100

    nuevo

---
+++ 3ªEva/P35b_CamposSeparadosADimensionados/P35b_CamposSeparadosADimensionados/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace P35b_CamposSeparadosADimensionados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = File.OpenText(@".\Datos\PacientesPeso_CS.txt");
+            StreamWriter sw = File.CreateText(@".\Datos\PacientesPeso_CD.txt");
+            string[] vCampos;
+            while (!sr.EndOfStream)
+            {
+                vCampos=sr.ReadLine().Split('$');
+                sw.Write(CuadraTexto(vCampos[0],3));
+                sw.WriteLine(CuadraTexto(vCampos[1], 28));
+
+            }
+            sr.Close();
+            sw.Close();      
+        }
+        static string CuadraTexto(string texto, int numChar)
+        {
+            texto += "                                               ";
+            return texto.Substring(0,numChar);
+        }
+
+    }
+}
